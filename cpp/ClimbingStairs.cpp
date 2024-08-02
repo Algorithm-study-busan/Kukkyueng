57a830d4d219dd814dfcd03db0e28a8a673d3a90
Kukkyeung <jungse8609@naver.com>
2024-08-02 21:14:25 +0900

    [LEET] 70. Climbing Staris / DP

---
+++ cpp/ClimbingStairs.cpp
@@ -0,0 +1,21 @@
+// LEET 70. Climbing Stairs
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[46];
+        dp[1] = 1;
+        dp[2] = 2;
+        // dp[n] = dp[n - 1] + dp[n - 2]
+
+        for (int i=3; i<=n; ++i)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
